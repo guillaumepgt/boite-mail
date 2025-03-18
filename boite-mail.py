603cdfd3edad76e7ea86a5c135774e4e2f4a0b42
Maxxprtt <maxime.peyrat@reseau.eseo.fr>
2025-03-18 11:59:48 +0100

    début de la fenetre

--- boite-mail.py
+++ boite-mail.py
@@ -1,9 +1,33 @@
 from tkinter import *
 
 fenetre = Tk()
-fenetre.title("TP Morpion")
-fenetre.geometry("-fullscreen", True)
-fenetre.iconbitmap("logo.ico")
+fenetre.title("Boite Mail")
+fenetre.attributes("-fullscreen", True)  # Active le mode plein écran
+fenetre.iconbitmap("logo.ico")  # Vérifiez que "logo.ico" existe
+
+"""# Quitter le plein écran avec Échap
+fenetre.bind("<Escape>", lambda event: fenetre.attributes("-fullscreen", False))"""
+
+
+
+
+
+# Création des 4 rectangles de menu
+
+
+can = Canvas(fenetre, width=200, height=800, bg="blue")
+can.place(x=100, y=100)
+
+
+
+
+
+
+
+
+
+
+
 
 
 fenetre.mainloop()
