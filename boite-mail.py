a3f1aa77101821cee3eb46d9fba488508034362c
guillaumepgt <guillaume.prigent44@gmail.com>
2025-05-07 15:37:18 +0200

    reglage de bug

--- boite-mail.py
+++ boite-mail.py
@@ -524,58 +524,6 @@ def envoye_mail():
         canvas.bind_all("<Button-4>", _on_mousewheel)
         canvas.bind_all("<Button-5>", _on_mousewheel)
 
-def envoye_mail():
-    global fenetre_boite, cache_images, canvas
-    canvas.delete("all")  # vide le contenu précédent du canvas
-    canvas.yview_moveto(0) # remettre le canvas en haut
-
-    # Frame dans le canvas pour contenir tous les boutons
-    frame_boite = Frame(canvas)
-    frame_boite.bind("<Configure>",lambda event: canvas.configure(scrollregion=canvas.bbox("all")))
-
-    # Créer une fenêtre dans le canvas
-    canvas_frame = canvas.create_window((0, 0), window=frame_boite, anchor="nw")
-
-    contact = lire_mail("envoye_name_list")
-    compteur = 0
-
-    frame_boite.config(width=largeur_ecran, height=hauteur_ecran*3)
-
-    for i in range(len(contact)):
-        use = 0
-        for j in range(i):
-            if contact[i]["Nom"] == contact[j]["Nom"]:
-                use = 1
-        if use == 0:
-            if contact[i]["Email"] not in cache_images :
-                cache_images = enregistrer_icone_tkinter(contact)
-            icone = cache_images[contact[i]["Email"]]
-            Button(frame_boite, compound="top", text=contact[i]["Nom"], image=icone, font=("Arial", 20),bg="lightblue", fg="black", relief="flat", activebackground="white", activeforeground="black",command=lambda email=contact[i]["Email"]: discussion(email)).place(
-                x=largeur_ecran * [0.05, 0.35, 0.65][compteur % 3],
-                y=hauteur_ecran * (0.3 * (compteur // 3)),
-                width=largeur_ecran * 0.2,
-                height=hauteur_ecran * 0.2
-            )
-            compteur += 1
-
-    def _on_mousewheel(event):
-        if platform.system() == 'Windows':
-            canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
-        elif platform.system() == 'Darwin':  # macOS
-            canvas.yview_scroll(int(-1 * event.delta), "units")
-        else:  # Linux
-            if event.num == 4:
-                canvas.yview_scroll(-1, "units")
-            elif event.num == 5:
-                canvas.yview_scroll(1, "units")
-
-    # Bind selon la plateforme
-    if platform.system() in ['Windows', 'Darwin']:
-        canvas.bind_all("<MouseWheel>", _on_mousewheel)
-    else:  # Linux
-        canvas.bind_all("<Button-4>", _on_mousewheel)
-        canvas.bind_all("<Button-5>", _on_mousewheel)
-
 def ecrire_mail_brouillon():
     global canvas, brouillon
 
